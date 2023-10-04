a5119fd00c7aaf7352374f2498d4cd8d0642a9db
adipurnomo0702021 <adipurnomo900@gmail.com>
2023-10-04 17:32:30 +0700

    First Commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const slideCount = slides.length;
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const visibleSlides = 5; // Jumlah slide yang akan ditampilkan pada suatu waktu
+
+function showSlide(index) {
+  slides.forEach((slide) => {
+    slide.style.display = 'none'; // Semua slide disembunyikan
+  });
+
+  for (let i = index; i < index + visibleSlides; i++) {
+    if (i < slideCount) {
+      slides[i].style.display = 'block'; // Menampilkan 5 slide berikutnya atau kurang
+    }
+  }
+}
+
+function nextSlide() {
+  if (currentSlide + visibleSlides < slideCount) {
+    currentSlide += 1;
+    showSlide(currentSlide);
+  }
+}
+
+function prevSlide() {
+  if (currentSlide > 0) {
+    currentSlide -= 1;
+    showSlide(currentSlide);
+  }
+}
+
+prevButton.addEventListener('click', prevSlide);
+nextButton.addEventListener('click', nextSlide);
+
+// Menampilkan 5 slide pertama saat halaman dimuat
+showSlide(currentSlide);
+
+
